8cff6b64db241efcc7e25cb563812adf042b13a8
Sanghoon Lee <dony.shlee@gmail.com>
2021-03-17 04:32:09 +0900

    add ribbon configuration

--- order-service/src/main/java/com/example/shopping/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/example/shopping/orderservice/OrderServiceApplication.java
@@ -1,12 +1,20 @@
 package com.example.shopping.orderservice;
 
+import com.example.shopping.orderservice.config.RibbonConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+/* */
+//@RibbonClient(name = "product-service", configuration = RibbonConfiguration.class)
+//@EnableEurekaClient
+@EnableDiscoveryClient
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
---
+++ order-service/src/main/java/com/example/shopping/orderservice/config/RibbonConfiguration.java
@@ -0,0 +1,24 @@
+package com.example.shopping.orderservice.config;
+
+import com.netflix.client.config.IClientConfig;
+import com.netflix.loadbalancer.AvailabilityFilteringRule;
+import com.netflix.loadbalancer.IPing;
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.PingUrl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+
+public class RibbonConfiguration {
+    @Autowired
+    IClientConfig iClientConfig;
+
+    @Bean
+    public IPing ping(IClientConfig iClientConfig) {
+        return new PingUrl();
+    }
+
+    @Bean
+    public IRule rule(IClientConfig iClientConfig) {
+        return new AvailabilityFilteringRule();
+    }
+}
